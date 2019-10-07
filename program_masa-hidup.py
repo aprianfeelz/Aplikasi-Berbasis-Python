65283b6818ebed96e592bf5971fb1c95e5d56655
Apriyan Fillah <aprian.feelz@gmail.com>
2019-10-07 13:59:09 -0700

    Create program_masa-hidup.py

---
+++ program_masa-hidup.py
@@ -0,0 +1,9 @@
+print("Mari kita lihat berapa umur Anda dalam hari, menit, dan detik.")
+name = input("Nama: ")
+print("Sekarang Masukkan Umur Anda")
+age = int(input("Umur: "))
+days = age * 365
+minutes = age * 525948     
+seconds = age * 31556926
+
+print(name, "telah hidup selama", days,"hari", minutes, "menit, dan", seconds, "detik! Wah!")
